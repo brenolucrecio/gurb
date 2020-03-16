125c7702346c4a33585e5891844ab60ac517d340
Breno <breno.lucrecio@clinicalup.com>
2020-03-15 23:45:49 -0300

    fist commit

---
+++ Gurb/View/frm_Main.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Gurb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbtText = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbCommand = new System.Windows.Forms.RichTextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.panel1.Controls.Add(this.tbtText);
+            this.panel1.Location = new System.Drawing.Point(12, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 271);
+            this.panel1.TabIndex = 0;
+            // 
+            // tbtText
+            // 
+            this.tbtText.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbtText.Location = new System.Drawing.Point(3, 3);
+            this.tbtText.Name = "tbtText";
+            this.tbtText.Size = new System.Drawing.Size(649, 265);
+            this.tbtText.TabIndex = 0;
+            this.tbtText.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.panel2.Controls.Add(this.tbCommand);
+            this.panel2.Location = new System.Drawing.Point(12, 310);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(655, 271);
+            this.panel2.TabIndex = 1;
+            // 
+            // tbCommand
+            // 
+            this.tbCommand.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCommand.Location = new System.Drawing.Point(3, 3);
+            this.tbCommand.Name = "tbCommand";
+            this.tbCommand.ReadOnly = true;
+            this.tbCommand.Size = new System.Drawing.Size(649, 265);
+            this.tbCommand.TabIndex = 1;
+            this.tbCommand.Text = "";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.Color.White;
+            this.btnConvert.Location = new System.Drawing.Point(673, 38);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(112, 50);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(685, 479);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(673, 94);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 50);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.White;
+            this.btnExport.Location = new System.Drawing.Point(673, 313);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(112, 50);
+            this.btnExport.TabIndex = 6;
+            this.btnExport.Text = "Exportar SQL";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.label1.Location = new System.Drawing.Point(367, 588);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "© Gurb 2020";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.label2.Location = new System.Drawing.Point(753, 591);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "v1.0.1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 610);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gurb";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox tbtText;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox tbCommand;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Gurb/View/frm_Main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gurb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            CreateCommand();
+        }
+
+        private void CreateCommand()
+        {
+            int cmdLenght = this.tbtText.Lines.Length;
+            for (int i = 0; i < cmdLenght; i++)
+            {
+                try
+                {
+                    string text = tbtText.Lines[i];
+                    if (!string.IsNullOrWhiteSpace(text))
+                    {
+                        var datatype = this.GetType(text.Substring(0, 2));
+                        var column = text.Substring(0, text.IndexOf('-') - 1);
+                        var table = text.Substring(text.IndexOf('-') + 1);
+
+                        var cmd = string.Format("ALTER TABLE {0} ADD {1} {2};\n", table.ToUpper(), column.ToUpper(), datatype);
+                        this.tbCommand.AppendText(cmd);
+                        this.ChangeColorCommand();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Formato de entrada inválida!\nVerifica as informações de entrada.", "Gurb", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string GetType(string text)
+        {
+            var datatype = "";
+            text = text.ToUpper();
+            switch (text)
+            {
+                case "CD":
+                    datatype = "INT";
+                    break;
+                case "DS":
+                    datatype = "NVARCHAR(MAX)";
+                    break;
+                case "NR":
+                    datatype = "DECIMAL";
+                    break;
+                case "VL":
+                    datatype = "DECIMAL";
+                    break;
+                case "X_":
+                    datatype = "BIT";
+                    break;
+                default:
+                    break;
+            }
+
+            return datatype;
+        }
+
+        private void ChangeColorCommand()
+        {
+            this.CheckKeyword("ALTER", Color.Blue, 0);
+            this.CheckKeyword("TABLE", Color.Blue, 0);
+            this.CheckKeyword("ADD", Color.Blue, 0);
+            this.CheckKeyword("BIT", Color.Blue, 0);
+            this.CheckKeyword("NVARCHAR", Color.Blue, 0);
+            this.CheckKeyword("INT", Color.Blue, 0);
+            this.CheckKeyword("DECIMAL", Color.Blue, 0);
+            this.CheckKeyword("MAX", Color.Purple, 0);
+        }
+
+        private void CheckKeyword(string word, Color color, int startIndex)
+        {
+            if (this.tbCommand.Text.Contains(word))
+            {
+                int index = -1;
+                int selectStart = this.tbCommand.SelectionStart;
+
+                while ((index = this.tbCommand.Text.IndexOf(word, (index + 1))) != -1)
+                {
+                    this.tbCommand.Select((index + startIndex), word.Length);
+                    this.tbCommand.SelectionColor = color;
+                    this.tbCommand.Select(selectStart, 0);
+                    this.tbCommand.SelectionColor = Color.Black;
+                }
+            }
+        }
+
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Em desenvolvimento...", "Gurb", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            this.tbCommand.Clear();
+            this.tbtText.Clear();
+            this.tbtText.Focus();
+        }
+    }
+}
